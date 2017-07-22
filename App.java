cbfabe601e2c47261d469dd6a833c15d4252d0ac
Lew3r <Lew3r@outlook.it>
2017-07-22 14:32:13 +0200

    Client

--- App.java
+++ App.java
@@ -1,7 +1,5 @@
 package Client;
 import java.io.IOException;
-import java.net.Socket;
-
 
 public class App 
 {
--- Lettura.java
+++ Lettura.java
@@ -1,7 +1,6 @@
 package Client;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
